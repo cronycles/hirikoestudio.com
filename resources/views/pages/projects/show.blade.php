78b2e681b5c16fe79863c265a5ad26b5299dd4f6
cronycles <cronycles@gmail.com>
2020-05-10 15:36:18 +0200

    ora se l'immagine é small si mette

--- resources/views/pages/projects/show.blade.php
+++ resources/views/pages/projects/show.blade.php
@@ -9,7 +9,7 @@
     @if($model->project->images != null && !empty($model->project->images))
         <div class="cro__vertical__gallery">
             @foreach($model->project->images as $image)
-                <div class="gallery__box">
+                <div class="gallery__box {{$image->isSmallViewEnabled ? 'small' : ''}}">
                     <div class="image__track">
                         <img src="{{config('custom.images.static.defaultLazyPlaceholder')}}" data-src="{{$image->url}}"
                              class="jlimg1000"/>
