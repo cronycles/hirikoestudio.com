1706ec2c3264d5a1e0676548c59719c56de1b6a2
cronycles <cronycles@gmail.com>
2020-05-12 19:01:27 +0200

    fix css

--- resources/views/custom/imagesUploader/_form.blade.php
+++ resources/views/custom/imagesUploader/_form.blade.php
@@ -5,22 +5,25 @@
      data-no-valid-image-err="{{trans('images-upload.no-valid-image')}}">
 
     <div class="img__upload_container">
-        <div class="img__upload_select_zone">
-            <ul class="jerrorListCont none">
+        <div class="page__section img__upload_select_zone">
+            <ul class="jerrorListCont page__section__box none">
                 <li class="jClonable none"></li>
             </ul>
 
-            <input type="file" class="jimgHandling__file-input-clonable" multiple accept="image/*" style="display:none">
-            <form class="juploadForm img__upload-select_form">
-                <a class="jselectFile cro__button cro__button--small" href="#">
+            <div class="page__section__box">
+                <input type="file" class="jimgHandling__file-input-clonable" multiple accept="image/*"
+                       style="display:none">
+                <form class="juploadForm img__upload-select_form">
+                    <a class="jselectFile cro__button cro__button--small" href="#">
                     <span class="cro__button__icon-and-text">
                          <i class="las la-upload"></i>
                         {{trans('images-upload.select-images-button')}}
                     </span>
 
-                </a>
-            </form>
-            <div>
+                    </a>
+                </form>
+            </div>
+            <div class="page__section__box">
                 <div class="jdropzone img__upload-dropzone">
                     <div class="img__upload-dropzone-text">
                         {{trans('images-upload.drag-drop-images')}}
@@ -28,11 +31,12 @@
                 </div>
             </div>
         </div>
-        <div class="img__upload_thumbnail_zone">
+        <div class="page__section img__upload_thumbnail_zone">
             <div class="img__upload_thumb-text">
                 {{trans('images-upload.thumbs-description')}}
             </div>
-            <ul id="thumbContainer" class="jThumbsContainer jsortableContainer img__upload__thumbs__container" data-url="{{$model->updateSortApiUrl}}">
+            <ul id="thumbContainer" class="jThumbsContainer jsortableContainer img__upload__thumbs__container"
+                data-url="{{$model->updateSortApiUrl}}">
                 @if($model->images != null && !empty($model->images))
                     @foreach ($model->images as $image)
                         <li class="jThumb jsortableElement img__upload__thumb-wrap" data-id="{{$image->id}}">
@@ -43,11 +47,14 @@
                                 <span class="img__upload__thumb-left__btn jSortHandle">
                                     <i class="las la-sort" title="{{trans('images-upload.sort-image-button')}}"></i>
                                 </span>
-                                <span class="jSmallView img__upload__thumb-center__btn {{$image->isSmallViewEnabled ? 'active' : ''}}">
-                                    <i class="la la-compress-arrows-alt" title="{{trans('images-upload.small-image-button')}}"></i>
+                                <span
+                                    class="jSmallView img__upload__thumb-center__btn {{$image->isSmallViewEnabled ? 'active' : ''}}">
+                                    <i class="la la-compress-arrows-alt"
+                                       title="{{trans('images-upload.small-image-button')}}"></i>
                                 </span>
                                 <span class="img__upload__thumb-right__btn">
-                                    <i class="jDel las la-trash-alt" title="{{trans('images-upload.delete-image-button')}}"></i>
+                                    <i class="jDel las la-trash-alt"
+                                       title="{{trans('images-upload.delete-image-button')}}"></i>
                                     <div class="jDelConfirm img__upload__confirm-btn none">
                                         {{trans('images-upload.delete-confirm-image-button')}}
                                     </div>
@@ -61,7 +68,8 @@
             <li class="jThumb jsortableElement jClonable img__upload__thumb-wrap none" data-id="0">
                 <img src="" class="jthumbImg">
 
-                <div class="jthumbPercentageBarContainer img__upload__thumb__buttons-wrap img__upload__progress-bar_container">
+                <div
+                    class="jthumbPercentageBarContainer img__upload__thumb__buttons-wrap img__upload__progress-bar_container">
                     <div class="jPercentageBar img__upload_progress-bar"></div>
                     <div class="jPercentageNumber img__upload_percentage-number">0%</div>
                 </div>
