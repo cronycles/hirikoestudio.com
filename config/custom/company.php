c6b1e0ff5ae4c5b415842907d8bb0c1a2d8fd01e
cronycles <cronycles@gmail.com>
2020-05-15 18:12:40 +0200

    classi per tagmanager pagina contatto

--- config/custom/company.php
+++ config/custom/company.php
@@ -7,5 +7,5 @@ return [
     'telephone' => '+34946853206',
     'telephone-txt' => '946 853 206',
     'email' => 'info@hirikoestudio.com',
-    'address-map-url' => 'https://goo.gl/maps/qGnMhsYXkNL2'
+    'address-map-url' => 'https://goo.gl/maps/pjbff8mdMzmvi63U8'
 ];
