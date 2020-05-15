2b289f92388dd5e2a79865ace8f998f09190c3e9
cronycles <cronycles@gmail.com>
2020-05-15 14:50:00 +0200

    la home ora ha un link a progetti

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="home-container">
-        <div class="gallery__box cro-fs-images-carousel">
-            <figure>
-                <img src="{{config('custom.images.static.defaultLazyPlaceholder')}}"
-                     class="tns-lazy-img"
-                     data-src="{{config('custom.images.static.homeSlide1')}}"
-                     alt="{{config('custom.company.name')}}">
-            </figure>
-            <figure>
-                <img src="{{config('custom.images.static.defaultLazyPlaceholder')}}"
-                     class="tns-lazy-img"
-                     data-src="{{config('custom.images.static.homeSlide2')}}"
-                     alt="{{config('custom.company.name')}}">
-            </figure>
-            <figure>
-                <img src="{{config('custom.images.static.defaultLazyPlaceholder')}}"
-                     class="tns-lazy-img"
-                     data-src="{{config('custom.images.static.homeSlide3')}}"
-                     alt="{{config('custom.company.name')}}">
-            </figure>
+    <a href="{{route('projects')}}">
+        <div id="home-container">
+            <div class="gallery__box cro-fs-images-carousel">
+                <figure>
+                    <img src="{{config('custom.images.static.defaultLazyPlaceholder')}}"
+                         class="tns-lazy-img"
+                         data-src="{{config('custom.images.static.homeSlide1')}}"
+                         alt="{{config('custom.company.name')}}">
+                </figure>
+                <figure>
+                    <img src="{{config('custom.images.static.defaultLazyPlaceholder')}}"
+                         class="tns-lazy-img"
+                         data-src="{{config('custom.images.static.homeSlide2')}}"
+                         alt="{{config('custom.company.name')}}">
+                </figure>
+                <figure>
+                    <img src="{{config('custom.images.static.defaultLazyPlaceholder')}}"
+                         class="tns-lazy-img"
+                         data-src="{{config('custom.images.static.homeSlide3')}}"
+                         alt="{{config('custom.company.name')}}">
+                </figure>
+            </div>
         </div>
-    </div>
+    </a>
 @endsection
 
 
