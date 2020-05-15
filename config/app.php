8b90152af130c20bddd1a521149fb4bca9156c61
cronycles <cronycles@gmail.com>
2020-05-15 17:02:49 +0200

    changing release version to: v2.21

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'version' => env('APP_VERSION', '2.20'),
+    'version' => env('APP_VERSION', '2.21'),
 
     /*
     |--------------------------------------------------------------------------
