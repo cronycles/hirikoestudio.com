488ca970ed57e55d57fabb2fac6aa09f6bdc61bd
cronycles <cronycles@gmail.com>
2020-05-15 07:18:32 +0200

    changing release version to: v2.2

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'version' => env('APP_VERSION', '2.1'),
+    'version' => env('APP_VERSION', '2.2'),
 
     /*
     |--------------------------------------------------------------------------
