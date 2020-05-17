aaf46f087159de8c85f210c96329f32a28d56501
cronycles <cronycles@gmail.com>
2020-05-17 19:52:18 +0200

    slug!

---
+++ app/Custom/Slug/Controllers/SlugController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Custom\Slug\Controllers;
+
+use App\Custom\Logging\AppLog;
+use App\Http\Controllers\Controller;
+
+abstract class SlugController extends Controller {
+
+    protected function getIdFromSlug($slug) {
+        try {
+
+            $matches = [];
+            preg_match('/([^-]*)$/', $slug, $matches);
+
+            $idWithText = $matches[1];
+
+            $outcome = str_replace("id", "", $idWithText);
+            return $outcome;
+
+        } catch (\Exception $e) {
+            AppLog::error($e);
+            return 0;
+        }
+
+    }
+
+}
