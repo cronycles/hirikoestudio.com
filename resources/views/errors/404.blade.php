ddd4ec67cabd1777043c0e2b2299b8e8665e61f5
cronycles <cronycles@gmail.com>
2020-05-21 19:37:12 +0200

    pagine di errore 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,4 @@
 @extends('errors::minimal')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+@section('title', __('page-errors-404.title'))
+@section('message', __('page-errors-404.message'))
