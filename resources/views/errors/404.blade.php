86c5d63e177e3e3d8deca1de6b91b98861a0a0ef
cronycles <cronycles@gmail.com>
2020-05-22 12:19:37 +0200

    fine 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,17 +1,9 @@
 @extends('layouts.page')
 
 @section('page_content')
-
-    <div class="page__section">
-        @if($model->title != null && !empty($model->title))
-            <div class="page__title">
-                <h1>{{$model->title}}</h1>
-            </div>
-        @endif
-        @if($model->description != null && !empty($model->description))
-            <div class="page__description">
-                <p>{{$model->description}}</p>
-            </div>
-        @endif
-    </div>
+    <section class="page__section page__unknown">
+        <p><i class="las la-frown-open"></i></p>
+        <h1>{{$model->title}} </h1>
+        <h2>{{$model->description}}</h2>
+    </section>
 @endsection
