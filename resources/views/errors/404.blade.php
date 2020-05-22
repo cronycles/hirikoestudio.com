2ede59d25e04cd87370417cd0f473941f36e0fa6
cronycles <cronycles@gmail.com>
2020-05-22 13:52:36 +0200

    fix 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,8 +2,11 @@
 
 @section('page_content')
     <section class="page__section page__unknown">
-        <p><i class="las la-frown-open"></i></p>
-        <h1>{{$model->title}} </h1>
-        <h2>{{$model->description}}</h2>
+        <div class='image'></div>
+        <div class="content">
+            <p><i class="las la-frown-open"></i></p>
+            <h1>{{$model->title}} </h1>
+            <h2>{{$model->description}}</h2>
+        </div>
     </section>
 @endsection
